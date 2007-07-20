062b209063e3d9e2499d2f885a1fff4d3e0ec4a4
Takanao.Endoh <Takanao.Endoh@680d1df1-232f-0410-a0f9-6b8e0ee6e22e>
2007-07-20 12:30:07 +0000

    sync to REL-2007-07-20-2115, r659.

--- trunk/helpdoc/tests.py
+++ trunk/helpdoc/tests.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-#markup_dispatch
+# find_source_file
+>>> from views import find_source_file
+>>> find_source_file("helpdoc/tests/index.txt")
+'helpdoc/tests/index.txt'
+>>> find_source_file("helpdoc/tests/index")
+'helpdoc/tests/index.txt'
+>>> find_source_file("helpdoc/tests/rst")
+'helpdoc/tests/rst.rst'
+>>> find_source_file("helpdoc/tests/markdown")
+'helpdoc/tests/markdown.markdown'
+>>> find_source_file("helpdoc/tests/textile")
+'helpdoc/tests/textile.textile'
+>>> find_source_file("helpdoc/tests/html")
+'helpdoc/tests/html.html'
+>>> print find_source_file("helpdoc/tests/none")
+None
+
+# get_source_file
+>>> from views import get_source_file
+
+# markup_dispatch
 >>> from views import markup_dispatch
 >>> markup_dispatch("./helpdoc/tests/index.txt").func_name
 'restructuredtext'
@@ -30,24 +50,23 @@ Django オンラインドキュメント和訳 : SITE TITLE
 >>> title("<div><h2>none-title<h2></div>", site_title)
 'Not Found Title Line. : SITE TITLE'
 
+>>> import context_processors
+>>> context = context_processors.helpdoc(None)
+>>> context.keys()
+['helpdoc_base_url']
+>>> url = context['helpdoc_base_url']
+
 >>> from django.core import management
 >>> from django.test.client import Client
 >>> management.load_data(["helpdoc/tests/auth.json"], verbosity=0)
->>> url = "/helpdoc/"
 >>> c = Client()
 >>> response = c.get(url)
 >>> response.status_code
 302
->>> response.headers["Location"]
-'/accounts/login/?next=/helpdoc/'
->>> c.login(username="test", password="secret")
-True
->>> response = c.get(url)
->>> response.status_code
-200
-
->>> import context_processors
->>> context_processors.helpdoc(None)
-{'helpdoc_base_url': '/helpdoc/'}
+>>> assert(response.headers["Location"].split("=")[1] == url)
+>>> version = management.get_version().startswith("0.97")
+>>> if version: assert(c.login(username="test", password="secret"))
+>>> if version: response = c.get(url)
+>>> if version: assert(response.status_code == 200)
 
 """
--- trunk/helpdoc/views.py
+++ trunk/helpdoc/views.py
@@ -1,25 +1,36 @@
 from django.http import Http404
 from django.views.generic.simple import direct_to_template
 from django.contrib.markup.templatetags.markup import textile, markdown, restructuredtext
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 import os.path
 import codecs
 
+def find_source_file(file_path):
+    if os.path.exists(file_path):
+        return file_path
+    file_path = os.path.splitext(file_path)[0]
+    for extension in ("txt","rst","markdown","textile","html"):
+        _file_path = "%s.%s" % (file_path, extension)
+        if os.path.exists(_file_path):
+            return _file_path
+    return None
+
 def get_source_file(file_path, encoding=None, **argv):
+    _file_path = find_source_file(file_path)
+    if not _file_path:
+        return None, None
     try:
         if encoding:
-            f = codecs.open(file_path, mode="r", encoding=encoding)
+            f = codecs.open(_file_path, mode="r", encoding=encoding)
         else:
-            f = open(file_path)
+            f = open(_file_path)
     except IOError:
-        return None
-    return f.read()
+        return None, None
+    return f.read(), _file_path
 
 def markup_dispatch(file_path, markup=None, **argv):
-    try:
-        extension = os.path.basename(file_path).split(".")[-1]
-    except IndexError:
-        extension = None
+    extension = os.path.splitext(file_path)[1][1:]
     markup_dict = dict(
         textile = textile,
         markdown = markdown,
@@ -37,10 +48,15 @@ def markup_dispatch(file_path, markup=None, **argv):
 def render(request, doc, app=None, file_path_pattern=None, base_url=None,
             template_name=None, **argv):
     app = app or ""
+    if not base_url:
+        if hasattr(settings, "HELPDOC_BASE_URL"):
+            base_url = settings.HELPDOC_BASE_URL
+        else:
+            base_url = ""
     template_name = template_name or "helpdoc/base_site.html"
 
     file_path = (file_path_pattern or "%s/docs/%s.txt") % (app, doc)
-    content = get_source_file(file_path, **argv)
+    content, file_path = get_source_file(file_path, **argv)
     if not content:
         raise Http404
     markup = markup_dispatch(file_path, **argv)
@@ -56,4 +72,5 @@ def index(request, base_url=None):
     if base_url:
         extra_context.update(dict(base_url=base_url))
     return direct_to_template(request, "helpdoc/index.html", extra_context=extra_context)
-index = permission_required("is_staff")(index)
+index = login_required(index)
+
